b40aa242c53dfdc152fffa3d5a9cbbb96afd6e6c
Stian Olsen <stianaolsen94@gmail.com>
2021-03-25 02:58:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+const cats = [
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
+];
+
 // Question 1
 const cat = {
   complain: function () {
@@ -50,20 +64,6 @@ resultsContainer.style.backgroundColor = "yellow";
 
 // Question 7
 
-const cats = [
-  {
-    name: "Blob",
-    age: 10,
-  },
-  {
-    name: "Harold",
-  },
-  {
-    name: "Blurt",
-    age: 21,
-  },
-];
-
 function kittens(list) {
   for (let i = 0; i < list.length; i++) {
     console.log(list[i].name);
@@ -75,9 +75,25 @@ kittens(cats);
 // Question 8
 
 function createCats(cats) {
+  let item = "";
+
   for (let i = 0; i < cats.length; i++) {
-    console.log(cats[i]);
+    let catAge = "Age unknown";
+
+    if (cats[i].age) {
+      catAge = cats[i].age;
+    }
+
+    item += `<div>
+            <h5>${cats[i].name}</h5>
+            <p>${catAge}</p>
+            </div>`;
   }
+
+  return item;
 }
 
-createCats(cats);
+const newHtml = createCats(cats);
+
+const container = document.querySelector(".cat-container");
+container.innerHTML = newHtml;
